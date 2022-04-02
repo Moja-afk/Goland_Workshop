c7bf1ad63bdd726869c2afd2fcd426881fe9ae17
Moja Williams <mojawilliams1@gmail.com>
2022-04-02 16:14:33 -0400

    trying to add

---
+++ tinder-clone/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Header from './Header';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is My attempt at an APP!</h1>
+
+      {}
+      <Header/>
+      {}
+      {}
+
+      {}
+      {}
+
+    </div>
+  );
+}
+
+export default App;
